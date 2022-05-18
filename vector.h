c81b068e331d784653d7245f4ed06fdc1bd6c51f
Anna <annabeths111@gmail.com>
2022-05-18 16:41:21 +0100

    added my existing vector library

---
+++ vector.h
@@ -0,0 +1,89 @@
+#ifndef ANNA_VECTORS
+#define ANNA_VECTORS
+
+#define PI 3.14152
+#define RAD2DEG 180 / PI
+#define DEG2RAD PI / 180
+
+#include <math.h>
+// unforunately this library requires the code to be linked
+// against the math library, due to needing `sqrt`
+
+typedef struct{
+	int x;
+	int y;
+}vec2;
+
+typedef struct{
+	float x;
+	float y;
+}vec2f;
+
+static void vec2fAdd(vec2f* dest, vec2f* src1, vec2f* src2);
+static float vec2fMagnitude(vec2f* v);
+static void vec2fDiff(vec2f* dest, vec2f* fromVector, vec2f* toVector);
+static float vec2fDist(vec2f* v1, vec2f* v2);
+
+static void vec2fAdd(vec2f* dest, vec2f* v1, vec2f* v2)
+{
+	dest->x = v1->x + v2->x;
+	dest->y = v1->y + v2->y;
+}
+
+static float vec2fDist(vec2f* v1, vec2f* v2)
+{
+	vec2f diffVec;
+	vec2fDiff(&diffVec, v1, v2);
+	return vec2fMagnitude(&diffVec);
+}
+
+static void vec2fDiff(vec2f* dest, vec2f* fromVector, vec2f* toVector)
+{
+	dest->x = toVector->x - fromVector->x;
+	dest->y = toVector->y - fromVector->y;
+}
+
+static float vec2fMagnitude(vec2f* v)
+{
+	return sqrt(pow(v->x, 2) + pow(v->y, 2));
+}
+
+static void vec2fScalarProduct(vec2f* dest, vec2f* v1, float scalar)
+{
+	dest->x = v1->x * scalar;
+	dest->y = v1->y * scalar;
+}
+
+static float vec2fAngleRadians(vec2f* fromVector, vec2f* toVector)
+{
+	vec2f diff;
+	vec2fDiff(&diff, fromVector, toVector);
+	return atan2(diff.y, diff.x);
+}
+
+static float vec2fAngleDegrees(vec2f* fromVector, vec2f* toVector)
+{
+	float rads = vec2fAngleRadians(fromVector, toVector);
+	return rads * RAD2DEG;
+}
+
+static void vec2fNormalise(vec2f* dest, vec2f* src)
+{
+	float length = vec2fMagnitude(src);
+	dest->x = src->x / length;
+	dest->y = src->y / length;
+}
+
+static void vec2fUnitVectorFromAngleDegrees(vec2f* dest, float angle)
+{
+	dest->x = cos(angle * DEG2RAD);
+	dest->y = sin(angle * DEG2RAD);
+}
+
+static void vec2fUnitVectorFromVector(vec2f* dest, vec2f* src)
+{
+	float magnitude = vec2fMagnitude(src);
+	dest->x = src->x / magnitude;
+	dest->y = src->y / magnitude;
+}
+#endif
